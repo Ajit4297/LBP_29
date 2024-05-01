87e28be0b5a3e009c1decc17ac4642a0c415d12d
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-01 07:21:10 +0530

    Add files via upload

---
+++ Untitled13.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+s="()()"
+c=0
+for i in range (len(s)):
+    if s[i]=="(" :
+        c=c+1
+    if s[i]==")":
+        c=c-1
+    
+print(c)   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
